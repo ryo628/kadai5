a4314677ff2b0fd45539789b35acdfc769f4eb30
j1317 <j1317@PC4-53.cc.matsue-ct.jp>
2017-09-26 15:06:19 +0900

    back images

--- main.rb
+++ main.rb
@@ -4,33 +4,48 @@ require 'dxruby'
 require_relative 'player'
 require_relative 'enemy'
 
-Window.width  = 800
-Window.height = 600
+# window size
+Window.width  = 640
+Window.height = 480
 
+# image
 player_img = Image.load("player.png")
 player_img.setColorKey([0, 0, 0])
 
 enemy_img = Image.load("enemy.png")
 enemy_img.setColorKey([0, 0, 0])
 
+back = Image.load("bg_outside_buildings.png")
+
+# font
 font = Font.new(16)
 
-player = Player.new(400, 500, player_img)
+# init player
+player = Player.new(400, 400, player_img)
 
+# init enemies
 enemies = []
 10.times do
- 	enemies << Enemy.new(rand(800), rand(600), enemy_img)
+ 	enemies << Enemy.new(rand(640), rand(480), enemy_img)
 end
 
+# main loop
 Window.loop do
+	# quit for press ESC
  	break if Input.keyPush?(K_ESCAPE)
 
+ 	# draw back image
+ 	Window.draw( 0, 0, back )
+
+ 	# enemies
 	Sprite.update(enemies)
 	Sprite.draw(enemies)
 
+	# player
 	player.update
 	player.draw
 
+	# shou hp
 	Window.draw_font( 10, 10, "HP : #{player.hp}", font )
 
 	# 当たり判定
