a90f53913c1a928e0a609f76b6126a9457ebefd3
j1317 <j1317@PC4-53.cc.matsue-ct.jp>
2017-09-26 14:55:00 +0900

    reindent

--- main.rb
+++ main.rb
@@ -13,22 +13,26 @@ player_img.setColorKey([0, 0, 0])
 enemy_img = Image.load("enemy.png")
 enemy_img.setColorKey([0, 0, 0])
 
+font = Font.new(16)
+
 player = Player.new(400, 500, player_img)
 
 enemies = []
 10.times do
-  enemies << Enemy.new(rand(800), rand(600), enemy_img)
+ 	enemies << Enemy.new(rand(800), rand(600), enemy_img)
 end
 
 Window.loop do
-  break if Input.keyPush?(K_ESCAPE)
+ 	break if Input.keyPush?(K_ESCAPE)
 
-  Sprite.update(enemies)
-  Sprite.draw(enemies)
+	Sprite.update(enemies)
+	Sprite.draw(enemies)
 
-  player.update
-  player.draw
+	player.update
+	player.draw
 
-  # 当たり判定
-  Sprite.check(player, enemies)
+	Window.draw_font( 10, 10, "HP : #{player.hp}", font )
+
+	# 当たり判定
+	Sprite.check(player, enemies)
 end
--- player.rb
+++ player.rb
@@ -1,8 +1,19 @@
 # coding: utf-8
 
 class Player < Sprite
-  def update
-    self.x += Input.x
-    self.y += Input.y
-  end
+	attr_accessor :hp
+
+ 	def initialize( *arg )
+ 		@hp = 5
+		super( *arg )
+	end
+
+	def update
+		self.x += Input.x
+		self.y += Input.y
+	end
+
+	def shot( obj )
+		@hp -= 1
+	end
 end
